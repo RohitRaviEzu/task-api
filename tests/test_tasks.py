d27159c9b88b90d9f19247eead9ea64d740271f8
Rohit Ravi <rohitravi2003@gmail.com>
2026-09-13 00:09:13 +0530

    test : CI Failure

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -34,7 +34,7 @@ client = TestClient(app)
 
 def test_health():
     response = client.get("/health")
-    assert response.status_code == 200
+    assert response.status_code == 999
     assert response.json() == {"status": "ok"}
 
 
